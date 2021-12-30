39e550e0eeb823afe88c7c0fe036d0d1f1d480a9
PATRICKjane7025 <95159108+PATRICKjane7025@users.noreply.github.com>
2021-12-30 11:50:14 +0530

    Add files via upload

---
+++ notes_20211230114839.py
@@ -0,0 +1,19 @@
+12.30 11:48 AM
+echo "# ctree" >> n=30
+for i in range(1,n+1):
+    print(' '*(n-i)+'* '*i)
+for i in range(int(n*0.45)):
+    for j in range(int(n-5)):
+        print('',end=' ')
+    print('* * * *')
+for i in range(n):
+    print('_ ',end='')
+print()
+print()
+print('~~~~~~~~~~~~HAPPY CHRISTMAS~~~~~~~~~~~~')
+print('~~~~~~~~~~~~~~~~~~AND~~~~~~~~~~~~~~~~~~')
+print('~~~~~~~~~~~~~HAPPY NEWYEAR~~~~~~~~~~~~~')
+print('Created by '
+      'ARAVIND RATHEESH -- '
+      'S5  ME -- '
+      'KMEA ENGINEERING COLLEGE')
